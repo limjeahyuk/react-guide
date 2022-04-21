0f2edc1762e3f06f79e924e20d11d1f759055e0c
혁쨩 <lim0202jh@naver.com>
2022-04-21 23:44:21 +0900

    11

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -5,4 +5,9 @@ const Card = (props) => {
   return <div className={classes}>{props.children}</div>;
 };
 
+const Card = (props) => {
+  const classes = 'card ' + props.className;
+  return <div className={classes}>{props.children}</div>;
+};
+
 export default Card;
