a40aa6789838637b01dc5eacd110a8c95e2c96ee
혁쨩 <lim0202jh@naver.com>
2022-04-25 02:08:57 +0900

    4.25

--- src/components/Expenses/Expenseltem.js
+++ src/components/Expenses/Expenseltem.js
@@ -3,6 +3,10 @@ import './ExpenseItem.css';
 import Card from '../UI/Card';
 
 const ExpenseItem = (props) => {
+  const clickHandler = () => {
+    console.log('cli');
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
@@ -10,6 +14,7 @@ const ExpenseItem = (props) => {
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
